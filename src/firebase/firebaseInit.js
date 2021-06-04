cd579c7afd22e2893f2975d8561608235b7e636e
lucasdgimenez <lucas_d360@hotmail.com>
2021-06-03 21:00:27 -0300

    page home

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import "firebase/firestore"
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA-Bmj82kbfepvX-Cc6PmnOuzwZFZ9snzo",
+  authDomain: "fireblog-ab6ff.firebaseapp.com",
+  projectId: "fireblog-ab6ff",
+  storageBucket: "fireblog-ab6ff.appspot.com",
+  messagingSenderId: "1043920932139",
+  appId: "1:1043920932139:web:d8b5ef7cbdd864e569896e"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+export default firebaseApp.firestore();
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <BlogPost :post="welcomeScreen"/>
+    <BlogPost :post="post" v-for="(post, index) in simpleBlogPost" :key="index"/>
+  </div>
+</template>
+
+<script>
+import BlogPost from '../components/BlogPost.vue'
+export default {
+  name: "Home",
+  components: {
+    BlogPost
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost: "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      simpleBlogPost: [
+        {
+          title: "This is a Filter Title!",
+          blogHTML: "This is a Filter blog post title!",
+          blogCoverPhoto: "beautiful-stories"
+        },
+         {
+          title: "This is a Filter Title 2!",
+          blogHTML: "This is a Filter blog post title!",
+          blogCoverPhoto: "designed-for-everyone"
+        }
+      ]
+    }
+  }
+};
+</script>
