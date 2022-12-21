42b6a1cdd55e1231c68f4b29d6d3a3886711b728
lucasdgimenez <lucas_d360@hotmail.com>
2022-12-21 01:16:10 -0300

    ForgetPassword

--- src/views/ForgetPassword.vue
+++ src/views/ForgetPassword.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    
+  <div class="reset-password">
+    <Modal v-if="modalActive" :close-modal="closeModal"/>
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot your passowrd? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import email from "../assets/Icons/envelope-regular.svg"
+import Modal from "../components/Modal"
 
+export default {
+  name: "ForgotPassword",
+  components:{email, Modal},
+  data() {
+    return {
+      email: null,
+      modalActive: false,
+      modalmessage: ""
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
 </style>
